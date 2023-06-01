6164943a0880d555b338bd20587c203a9c1c20bb
nrsfqh <syafiqah2462@gmail.com>
2023-06-01 14:01:32 +0800

    first commit

---
+++ recipes.js
@@ -0,0 +1,5 @@
+const recipes = [
+    { id: 1, title: 'Nasi Lemak', ingredients: ['rice', 'coconut milk', 'anchovies', 'peanuts', 'cucumber', 'sambal'], cuisine: 'Malaysian' },
+    { id: 2, title: 'Fish & Chips', ingredients: ['Fish fillets', 'Potatoes ','All-purpose flour','Baking powder'], cuisine: 'Western' },
+    { id: 3, title: 'Recipe 3', ingredients: ['potatoes', 'tomatoes'], cuisine: 'Malaysian'}
+];
