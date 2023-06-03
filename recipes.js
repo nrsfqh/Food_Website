d2e7e500c94afaba27832f3efced1627c3c81a5a
nrsfqh <syafiqah2462@gmail.com>
2023-06-03 12:57:05 +0800

    updated the html layout and recipe array

--- recipes.js
+++ recipes.js
@@ -1,5 +1,7 @@
 const recipes = [
     { id: 1, title: 'Nasi Lemak', ingredients: ['rice', 'coconut milk', 'anchovies', 'peanuts', 'cucumber', 'sambal'], cuisine: 'Malaysian' },
     { id: 2, title: 'Fish & Chips', ingredients: ['Fish fillets', 'Potatoes ','All-purpose flour','Baking powder'], cuisine: 'Western' },
-    { id: 3, title: 'Recipe 3', ingredients: ['potatoes', 'tomatoes'], cuisine: 'Malaysian'}
+    { id: 3, title: 'Spaghetti Bolognese', ingredients: ['spaghetti', 'ground beef', 'onion', 'garlic', 'tomato sauce', 'tomato paste', 'dried oregano', 'dried basil', 'salt', 'black pepper', 'olive oil'], 
+    cuisine: 'Western'},
+    { id: 4, title: 'Nasi Ayam', ingredients: ['chicken', 'rice', 'soy sauce', 'ginger', 'garlic', 'cucumber', 'chili sauce'], cuisine: 'Malaysian'}
 ];
