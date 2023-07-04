9e1eeec2afdfafce4cada55d7077e45448baba4a
nrsfqh <syafiqah2462@gmail.com>
2023-07-04 12:31:13 +0800

    Modified Codes + Made a few touch-ups

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { ObjectId } = require('mongodb');
-const {connectToDb,getDb} = require('./db');
+const { connectToDb, getDb } = require('./db');
 
 
 //initialise app
@@ -13,57 +13,16 @@ app.use(express.static('public'));
 let db;
 
 connectToDb((err) => {
-    if(!err){
+  if (!err) {
 
-        app.listen(3000, function(){
+    app.listen(3000, function () {
 
-            console.log("Server is running")
-        })
+      console.log("Server is running")
+    })
 
-        db = getDb()
-    }
+    db = getDb()
+  }
 })
-
-
-//Routes
-
-// app.get('/recipes',(req,res) => {
-//     let recipes = []
-
-//     db.collection('recipes')
-//         .find()
-//         .sort({title: 1})
-//         .forEach(recipe => recipes.push(recipe))
-//         .then(() => {
-//             console.log("Recipes fetched successfully");
-//              res.send(recipes); // Send the fetched recipes as the response
-//          })
-//          .catch(error => {
-//              console.log("Error fetching recipes:", error);
-//              res.status(500).send("Error fetching recipes"); // Send an error response if there's an issue with fetching recipes
-//          });
-// })
-
-// app.get('/recipes/:id', (req, res) => {
-
-//     if(ObjectId.isValid(req.params.id)){
-
-//         db.collection('recipes')
-//             .findOne({_id: new ObjectId(req.params.id)})
-//             .then(doc => {
-//                 res.status(200).json(doc)
-//             })
-//             .catch(err => {
-//                 res.status(500).json({error: "Could not fetch Document"})
-
-//             })
-//     } else{
-
-//         res.status(500).json({error: "Not valid doc id"})
-//     }
-    
-    
-// })
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
@@ -71,41 +30,45 @@ app.get('/', (req, res) => {
 
 //Recipe arrays
 app.get('/recipes', (req, res) => {
-    db.collection('recipes_list') // Update the collection name to "recipes_list"
-      .find()
-      .sort({ title: 1 })
-      .toArray()
-      .then((recipes) => {
-        //console.log('Recipes fetched successfully:', recipes);
-        console.log('Recipes fetched successfully:');
-        res.status(200).json(recipes); // Send the fetched recipes as the response
-      })
-      .catch((error) => {
-        console.log('Error fetching recipes:', error);
-        res.status(500).json({ error: 'Error fetching recipes' }); // Send an error response if there's an issue with fetching recipes
-      });
-  });
-  
-//Filters based on ID
+  //This is the mongodb collection name
+  db.collection('recipes_list') 
+    .find()
+    .sort({ title: 1 })
+    .toArray()
+    .then((recipes) => {
+      //console.log('Recipes fetched successfully:', recipes);
+      console.log('Recipes fetched successfully:');
+      // Send the fetched recipes as the response
+      res.status(200).json(recipes); 
+    })
+    .catch((error) => {
+      console.log('Error fetching recipes:', error);
+      // Send an error response IF there's an issue with fetching recipes
+      res.status(500).json({ error: 'Error fetching recipes' }); 
+    });
+});
+
+//Filters based on ID (to get the indv ingredients, steps, ect... for each recipe)
 app.get('/recipes/:id', (req, res) => {
-    if (ObjectId.isValid(req.params.id)) {
-      db.collection('recipes_list') // Update the collection name to "recipes_list"
-        .findOne({ _id: new ObjectId(req.params.id) })
-        .then((doc) => {
-          if (doc) {
-            res.status(200).json(doc);
-          } else {
-            res.status(404).json({ error: 'Recipe not found' });
-          }
-        })
-        .catch((err) => {
-          res.status(500).json({ error: 'Could not fetch document' });
-        });
-    } else {
-      res.status(400).json({ error: 'Invalid document ID' });
-    }
-  });
-  
+  if (ObjectId.isValid(req.params.id)) {
+    // mongodb collection name
+    db.collection('recipes_list') 
+      .findOne({ _id: new ObjectId(req.params.id) })
+      .then((doc) => {
+        if (doc) {
+          res.status(200).json(doc);
+        } else {
+          res.status(404).json({ error: 'Recipe not found' });
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ error: 'Could not fetch document' });
+      });
+  } else {
+    res.status(400).json({ error: 'Invalid document ID' });
+  }
+});
+
 
 // Filtering based on cuisine
 app.get('/recipes/cuisine_filter/:cuisines', (req, res) => {
@@ -128,23 +91,22 @@ app.get('/recipes/cuisine_filter/:cuisines', (req, res) => {
 //Filter based on Ingredients
 app.get('/recipes/ingredients/:ingredients', (req, res) => {
   const ingredients = req.params.ingredients.split(',');
-  
 
-  // Remove any leading or trailing whitespace from each ingredient
+  // Remove any whitespace from each ingredient
   const cleanedIngredients = ingredients.map((ingredient) => ingredient.trim());
 
   // Query the database using ingredients and return the matching recipes
   db.collection('recipes_list')
-    .find({ ingredients: { $all: cleanedIngredients  } })
+    .find({ ingredients: { $all: cleanedIngredients } })
     .sort({ title: 1 })
     .toArray()
     .then((recipes) => {
       console.log('Recipes fetched successfully:', recipes);
-      res.status(200).json(recipes); // Send the fetched recipes as the response
+      res.status(200).json(recipes); 
     })
     .catch((error) => {
       console.log('Error fetching recipes:', error);
-      res.status(500).json({ error: 'Error fetching recipes' }); // Send an error response if there's an issue with fetching recipes
+      res.status(500).json({ error: 'Error fetching recipes' });
     });
 });
 
@@ -175,6 +137,15 @@ app.get('/recipes/filter/:ingredients/:cuisines', (req, res) => {
       console.log('Error fetching recipes:', error);
       res.status(500).json({ error: 'Error fetching recipes' });
     });
+});
+
+//Get Recipe images
+app.get('/img/:imageUrl', (req, res) => {
+  // Read the image file from project directory using the imageUrl
+  const imageUrl = req.params.imageUrl;
+
+  // Send it as the response
+  res.sendFile(__dirname + '/img/' + imageUrl);
 });
 
 
